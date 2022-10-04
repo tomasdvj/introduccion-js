239f13b09b8523d2ab94c790e0a019a52839c4ff
Tomas Vergara <tomas.d.v.j@hotmail.com>
2022-10-04 12:56:55 -0300

    Variables

---
+++ js/01.js
@@ -0,0 +1,19 @@
+//! Variables
+
+var producto = 'Audifonos Gamers'; //! var no se usa
+var disponible; //! Se pueden iniciar variables sin valor
+producto = true; //! Se puede reasignar valor de la variable
+
+var producto1 = 'Computadora',
+    disponible1  = true,
+    categoria = 'Computadores'; //! Se pueden inicializar varias variables a la vez
+
+//! No se puede inicializar una variable que empiece con un numero ej. 3disponible;
+
+//! Estilos para las variables
+var nombre_producto = 'Monitor HD'; //? Underscore
+var nombreProducto = 'Monitor HD'; //? Camelcase (la mas utilizada para crear variables)
+var NombreProducto = 'Monitor HD'; //? Pascalcase
+var nombreproducto = 'Monito HD'; //? lowercase
+
+//! Las variables son case sensitive (sensibles a las mayusculas y minusculas)
