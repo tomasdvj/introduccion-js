2972616603ea4e53c9af6d48f7f533f2598be2c2
Tomas Vergara <tomas.d.v.j@hotmail.com>
2022-10-04 15:16:14 -0300

    Operaciones matematicas

---
+++ js/05.js
@@ -0,0 +1,11 @@
+//! Numeros
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 200.20;
+const numero4 = -200;
+
+
+console.log(numero1 + numero2);
+console.log(numero2 * numero1);
+console.log(numero3);
+console.log(numero4);
