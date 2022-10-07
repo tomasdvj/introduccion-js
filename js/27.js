9042e028aa0e3b87c5701f37262e183c9a58581a
Tomas Vergara <tomas.d.v.j@hotmail.com>
2022-10-07 12:38:22 -0300

    Prototype

---
+++ js/27.js
@@ -0,0 +1,29 @@
+//! POO
+
+//? Object literal
+const producto = {
+    nombre: 'Tomas',
+    precio: 200
+}
+
+//? Object constructor
+function Producto(nombre,precio) {
+    this.nombre = nombre;
+    this.precio = precio;
+}
+
+//! Crear funciones  que solo se utilizan en unobjeto especifico
+Producto.prototype.formatearProducto = function() {
+    return `El producto ${this.nombre} tiene un precio de: ${this.precio}`;
+}
+
+const producto2 = new Producto('Tomas', 300);
+const producto3 = new Producto('ola', 5000);
+
+function formatearProducto(producto){
+    return `El producto ${producto.nombre} tiene un precio de: ${producto.precio}`;
+}
+
+console.log(producto2.formatearProducto());
+console.log(producto3);
+
